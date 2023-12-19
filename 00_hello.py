57c8ade09467ae9e3f93c8f20633db29cc5e4310
Your Name <you@example.com>
2023-12-19 19:40:21 +0100

    archivo ejemplo variables

--- 00_hello.py
+++ 00_hello.py
@@ -1,13 +1,3 @@
-# esto es un comentario: Hola mundo
-print("Hola python")
-print('Hola python con comillas simples')
-
-#consultar tipo de dato usamos type()
-print(type("hola mundo"))# soy un dato str
-print(type(2))#soy un dato int
-print(type(2.5))#soy un dato tipo float
-print(type(False))#soy un dato bool
-
 """ 
 esto es un comentario en 
 varias lineas para ello he puesto tres comilla dobles al inicio y al final
@@ -16,4 +6,12 @@ varias lineas para ello he puesto tres comilla dobles al inicio y al final
 esto tambien es un comentario
 en varias lineas poniendo comillas simples
 '''
+# esto es un comentario en una sola linea : Hola mundo
+print("Hola python")
+print('Hola python con comillas simples')
 
+#consultar tipo de dato usamos type() para saber si es de tipo texto, numero.. lo que ponemos dentro de type
+print(type("hola mundo"))# soy un dato str
+print(type(2))#soy un dato int
+print(type(2.5))#soy un dato tipo float
+print(type(False))#soy un dato bool
---
+++ 01_variables.py
@@ -0,0 +1,25 @@
+# variables
+
+mi_variable="mi variables de string"
+print(mi_variable)
+
+mi_int_vatiable=5
+print(mi_int_vatiable)
+print(type(mi_int_vatiable))
+
+mi_int_str_variable= str(mi_int_vatiable)
+print(mi_int_str_variable)
+print(type(mi_int_str_variable))
+
+mi_bool_variable=False
+print(mi_variable)
+
+#concadenacion de variables se usa la , en el print
+print(mi_variable,mi_int_vatiable,mi_bool_variable)
+
+#funciones del sistema: len() dice cuanto caracteres tiene la variable
+print(len(mi_variable))
+
+#variable en una sola linea
+nombre,apellido,alias,edad="Rosa","Garcia","rg",12
+print("mi alias es",alias,"mi edad es",edad,"mi nombre y apellido es",nombre,apellido)
