16f237b6d92ff03d421bc4e66d54926145db8a70
Your Name <you@example.com>
2023-12-19 19:00:22 +0100

    Fichero hello completado

---
+++ 00_hello.py
@@ -0,0 +1,19 @@
+# esto es un comentario: Hola mundo
+print("Hola python")
+print('Hola python con comillas simples')
+
+#consultar tipo de dato usamos type()
+print(type("hola mundo"))# soy un dato str
+print(type(2))#soy un dato int
+print(type(2.5))#soy un dato tipo float
+print(type(False))#soy un dato bool
+
+""" 
+esto es un comentario en 
+varias lineas para ello he puesto tres comilla dobles al inicio y al final
+"""
+''' 
+esto tambien es un comentario
+en varias lineas poniendo comillas simples
+'''
+
