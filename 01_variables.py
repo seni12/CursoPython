816b88cdcb68d93fa20e78558305ea094135ee71
seni12 <inesmariainf@gmail.com>
2023-12-20 14:07:22 +0100

    avance en variables

--- 01_variables.py
+++ 01_variables.py
@@ -23,3 +23,21 @@ print(len(mi_variable))
 #variable en una sola linea
 nombre,apellido,alias,edad="Rosa","Garcia","rg",12
 print("mi alias es",alias,"mi edad es",edad,"mi nombre y apellido es",nombre,apellido)
+
+#para que te pida los datos por consola y esos datos se guarde en variables
+"""
+nombre= input ("¿Cual es tu nombre?")
+apellido= input ("¿cual es tu apellido?")
+"""
+print("Tu nombre es:", nombre,"tu apellido es",apellido)
+
+#cambio de tipo
+nombre= 35
+edad = "rosa"
+print(nombre,edad)
+
+#forzamos tipo que sea string
+direccion: str = "Mi direccion"
+print(direccion)
+direccion=32
+print(type(direccion))
