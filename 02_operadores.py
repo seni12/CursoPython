0d73d7af23d059232e8e57c2c8500096565aa05f
seni12 <inesmariainf@gmail.com>
2023-12-22 13:21:48 +0100

    creacion de clase operadores

---
+++ 02_operadores.py
@@ -0,0 +1,13 @@
+#OPERADORES
+print (3 + 4)
+print (3 - 4)
+print (3 * 4)
+print (3 / 4)
+print (10 % 3) #Muestra el resto de una division
+print (10 // 3) # Redondeo de una dividon
+print (2 ** 3)# dos elevado a 3
+
+print("hola" + "Python ") # se utiliza para concadenar cadena de textos
+print("Hola"+ str(5)) #concatenar texto con numero.
+
+print("hola "*5)
